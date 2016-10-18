2fb4db093d26367d00513ef1c0ad88267af22af6
Fredrik Boström <fredrik@fredrikbostrom.net>
2016-10-18 10:50:37 +0300

    Change https to http for CDN resources in karma

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.12/angular.js',
-      'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.12/angular-mocks.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.12/angular.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.12/angular-mocks.js',
       'http://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.3.0/lodash.js',
       'src/restangular.js',
       'test/*.js'
--- karma.underscore.conf.js
+++ karma.underscore.conf.js
@@ -11,8 +11,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.6/angular.js',
-      'https://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.6/angular-mocks.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.6/angular.js',
+      'http://ajax.googleapis.com/ajax/libs/angularjs/1.3.0-beta.6/angular-mocks.js',
       'http://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.1/underscore.js',
       'src/restangular.js',
       'test/*.js'
